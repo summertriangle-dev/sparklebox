063c17ec1604208427ee287ae6563eb52103f0d0
karin <summertriangle.dev@gmail.com>
2015-12-03 14:24:06 -0800

    fix swap costume for spread

--- static/js/level.js
+++ static/js/level.js
@@ -12,16 +12,16 @@ function reassign_vars_for_skill(that) {
     var lv = parseInt(that.value) || 0;
     for (var i = 0; i < elems.length; i++) {
         assign_minmax_if_needed(elems[i]);
-        
+
         if (lv == 0) {
             elems[i].textContent = elems[i].getAttribute("data-min") + ".." + elems[i].getAttribute("data-max");
             continue;
         }
-        
+
         var add = (parseFloat(elems[i].getAttribute("data-max")) - parseFloat(elems[i].getAttribute("data-min"))) * ((lv - 1) / 9)
         var fr = parseFloat(elems[i].getAttribute("data-min")) + add;
         var trunc = fr | 0;
-        
+
         if (fr - trunc < 0.001) {
             elems[i].textContent = trunc;
         } else {
@@ -36,12 +36,12 @@ function reassign_vars_for_base(that) {
     var lv = parseInt(that.value) || 0;
     for (var i = 0; i < elems.length; i++) {
         assign_minmax_if_needed(elems[i]);
-        
+
         if (lv == 0) {
             elems[i].textContent = elems[i].getAttribute("data-min") + ".." + elems[i].getAttribute("data-max");
             continue;
         }
-        
+
         var add = (parseFloat(elems[i].getAttribute("data-max")) - parseFloat(elems[i].getAttribute("data-min"))) * ((lv - 1) / (parseInt(that.parentNode.getAttribute("data-stepper-max")) - 1))
         var fr = parseFloat(elems[i].getAttribute("data-min")) + add;
         elems[i].textContent = fr | 0;
@@ -53,7 +53,7 @@ function skill_onchange(that) {
     while (!twaddle.hasAttribute("data-stepper-max")) {
         twaddle = twaddle.parentNode;
     }
-    
+
     that.value = Math.min(parseInt(twaddle.getAttribute("data-stepper-max")), Math.max(0, parseInt(that.value))) || "All";
     reassign_vars_for_skill(that);
 }
@@ -63,7 +63,7 @@ function base_onchange(that) {
     while (!twaddle.hasAttribute("data-stepper-max")) {
         twaddle = twaddle.parentNode;
     }
-    
+
     that.value = Math.min(parseInt(twaddle.getAttribute("data-stepper-max")), Math.max(0, parseInt(that.value))) || "All";
     reassign_vars_for_base(that);
 }
@@ -101,7 +101,7 @@ function toggle_transform_state(that, owner) {
 
 function set_stats_visible(that, cid) {
     var root = that;
-    
+
     var statboxes = root.querySelectorAll(".stats.box");
     for (var i = 0; i < statboxes.length; i++) {
         if (statboxes[i].id == "sb_" + cid) {
@@ -110,28 +110,27 @@ function set_stats_visible(that, cid) {
             statboxes[i].style.display = "none";
         }
     }
-    
+
     var cardim = root.querySelector(".card_image");
     if (cardim !== null) {
         link = cardim.src.substring(0, cardim.src.lastIndexOf("/"));
         cardim.src = link + "/" + cid + ".png"
     } else {
         cardim = root.querySelector(".spread_view");
-        link = cardim.style.backgroundImage.substring(4, cardim.style.backgroundImage.length - 1);
-        link = link.substring(0, link.lastIndexOf("/"))
-        cardim.style.backgroundImage = "url(" + link + "/" + cid + ".png)";
+        link = cardim.style.backgroundImage.match(/url\("?(.+)\/(.+?).png"?\)/);
+        cardim.style.backgroundImage = "url(\"" + link[1] + "/" + cid + ".png\")";
     }
-    
+
     var sprite = root.querySelector(".sprite_link");
     if (sprite) {
         sprite.href = "/sprite_go/" + cid + ".png"
     }
-    
+
     var spread = root.querySelector(".spread_link");
     if (spread) {
         link = spread.href.substring(0, spread.href.lastIndexOf("/"));
         spread.href = link + "/" + cid + ".png"
     }
-    
+
     root.setAttribute("data-showing-id", cid)
 }
