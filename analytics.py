4c28bb4a8dcc9af892950440d6e62e750d7ec6ae
karin <summertriangle.dev@gmail.com>
2016-04-25 17:49:28 -0700

    fix import

--- analytics.py
+++ analytics.py
@@ -1,3 +1,5 @@
+import tornado.httputil
+
 class Analytics(object):
     """ Called by most endpoints. Log, analyze, whatever here. """
     def analyze_request(self, request: tornado.httputil.HTTPServerRequest,
