5fb1a6f026438afe16f6237b195fb1a71104918a
Fumii Chuu <gaiban@poketb.com>
2016-10-14 19:55:10 +0800

    Update get_app_ver.py
    tried my best

--- toolchain/get_app_ver.py
+++ toolchain/get_app_ver.py
@@ -1,41 +1,18 @@
-from html.parser import HTMLParser
 import urllib.request
+import json
+import os
 
-def getHtml(url):
-    page = urllib.request.urlopen(url)
-    html = page.read()
-    return html
-    
-class MyHTMLParser(HTMLParser):
-    get_version = False
-    
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.version = "1.9.1"
-    def handle_starttag(self, tag, attrs):
-        if tag == "span":
-            if len(attrs) == 0:
-                pass
-            else:
-                for (variable, value) in attrs:
-                    if (variable == "itemprop" and value == "softwareVersion"):
-                        self.get_version = True
-                        
-    def handle_endtag(self,tag):
-        if tag == 'span':
-            self.get_version = False
-              
-    def handle_data(self,data):
-        if self.get_version:
-            self.version = data
-            
-if __name__ == "__main__":
-    
-    store_b = getHtml("https://itunes.apple.com/jp/app/aidorumasuta-shindereragaruzu/id1016318735?mt=8")
-    store = store_b.decode('utf-8')
-    
-    proc = MyHTMLParser()
-    proc.feed(store)
-    proc.close()
-    print("get app_ver:", proc.version)
-    
+store_api = urllib.request.urlopen("https://itunes.apple.com/jp/lookup?id=1016318735")
+store_b = store_api.read()
+store_j = store_b.decode('utf-8')
+store_d = json.loads(store_j)
+
+results_l = store_d.get("results")
+results_j = json.dumps(results_l[0])
+results_d = json.loads(results_j)
+
+app_ver = results_d['version']
+
+print("get app_ver:", app_ver)
+os.environ["VC_APP_VER"] = app_ver
+print("set VC_APP_VER to", os.environ["VC_APP_VER"])
