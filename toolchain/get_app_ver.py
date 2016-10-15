634a73171c25d4a20c5d27bfe60e7e52576572f5
Fumii Chuu <gaiban@poketb.com>
2016-10-15 18:15:28 +0800

    Update get_app_ver.py

--- toolchain/get_app_ver.py
+++ toolchain/get_app_ver.py
@@ -7,9 +7,6 @@ store_j = store_b.decode('utf-8')
 store_d = json.loads(store_j)
 
 results_l = store_d.get("results")
-results_j = json.dumps(results_l[0])
-results_d = json.loads(results_j)
-
-app_ver = results_d['version']
+app_ver = results_l[0]['version']
 
 print(app_ver)
