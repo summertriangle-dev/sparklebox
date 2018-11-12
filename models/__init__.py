d6d60a9c68062a5ddf571c7a6d3309977e696b30
karin <summertriangle.dev@gmail.com>
2018-11-11 23:59:00 -0800

    ignore dupes in gacha_available_ex

--- models/__init__.py
+++ models/__init__.py
@@ -169,7 +169,7 @@ class TranslationSQL(object):
     def add_reward_tracking_entries(self, iterator):
         with self as s:
             for ent in iterator:
-                s.add(GachaRewardEntry(gacha_id=ent[0], step_num=ent[1], reward_id=ent[2], recommend_order=ent[3], limited_flag=ent[4]))
+                s.merge(GachaRewardEntry(gacha_id=ent[0], step_num=ent[1], reward_id=ent[2], recommend_order=ent[3], limited_flag=ent[4]))
             s.commit()
 
     @retry(5)
